889fe9e60e34583734fba3157c2e98f50ff1bf80
Alex Roehm <alex@roehms.de>
2023-08-08 11:50:53 +0200

    route app through dotnet

--- backend/Program.cs
+++ backend/Program.cs
@@ -6,6 +6,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSpaStaticFiles(config =>
+{
+    config.RootPath = "dist";
+});
+
 
 var app = builder.Build();
 
@@ -21,5 +26,15 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseSpaStaticFiles();
+
+app.UseSpa(builder =>
+{
+    if (app.Environment.IsDevelopment())
+    {
+        builder.UseProxyToSpaDevelopmentServer("http://localhost:5173");
+    }
+});
 
 app.Run();
